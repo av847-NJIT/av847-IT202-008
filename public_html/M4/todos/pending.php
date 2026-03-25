7e242acf4dca924bc62c92b7daeb3fc338a6a118
Angel Vera <avera@VIP.COM>
2026-03-24 20:45:57 -0400

    Completed querys for pending page

--- public_html/M4/todos/pending.php
+++ public_html/M4/todos/pending.php
@@ -12,8 +12,8 @@ if (isset($_POST["id"])) {
     Add an extra clause to update only if the complete field of the record is not set.
     https://phpdelusions.net/pdo
     */
-    $query = ""; // edit this
-    $params = []; // apply mapping
+    $query = "UPDATE M4_TODOS SET is_complete = 1, completed = CURRENT_TIMESTAMP WHERE id = :id AND is_complete = 0"; // edit this
+    $params = [":id" => $id]; // apply mapping
     
     try {
         $stmt = $db->prepare($query);
@@ -35,7 +35,7 @@ For Actions, this isn't part of the query and there's nothing special to select 
 Filter the results where the todo item is NOT completed and order the results by those due the soonest.
 No limit is required.
 */
-$query = ""; // edit this
+$query = "SELECT id, task, due, DATEDIFF(due, CURRENT_DATE) AS days_offset, assigned FROM M4_TODOS WHERE is_complete = 0 ORDER BY DUE ASC"; // edit this
 $results = [];
 try {
     $stmt = $db->prepare($query);
