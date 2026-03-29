8f5639916f7b99bd6674cf1bb6423718cecaa38f
Angel Vera <avera@VIP.COM>
2026-03-29 15:50:51 -0400

    updated email validation using filter var

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -41,6 +41,13 @@ if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
         $hasError = true;
     }
 
+    // Sanitize and validate email
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address<br>";
+        $hasError = true;
+    }
+
     if (empty($password)) {
         echo "Password must not be empty<br>";
         $hasError = true;
