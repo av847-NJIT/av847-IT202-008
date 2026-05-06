84ceaac1c8ab3b6c26a0cdd47f1504d022f02320
Angel Vera <avera@VIP.COM>
2026-05-06 19:37:49 -0400

    Updated landing.php page and added styling

--- public_html/project/landing.php
+++ public_html/project/landing.php
@@ -2,12 +2,19 @@
 require(__DIR__ . "/../../partials/nav.php");
 error_log("Session: " . var_export($_SESSION, true));
 ?>
-<h1>Landing Page</h1>
+<<div class="container mt-5 text-center">
+    <h1 class="display-4 mb-3">Home Page</h1>
 
-<?php if (is_logged_in(true)): ?>
-    <p>Welcome, <?php echo get_username() ?>!</p>
-<?php endif;?>
+    <p class="lead text-muted w-75 mx-auto">
+        Explore your favorite UFC fighter's offensive and defensive stats and compare them with others!
+    </p>
 
+    <?php if (is_logged_in(true)): ?>
+        <p class="fw-semibold">
+            Welcome, <?php echo get_username() ?>!
+        </p>
+    <?php endif;?>
+</div>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
